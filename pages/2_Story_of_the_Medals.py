dbf061c2c230576a913070cd7c7d644df6d3466a
Nivantha Bandara <gtbandara@Nivanthas-MacBook-Pro.local>
2024-08-28 20:05:10 +1000

    fixing bugs in visualisations

--- pages/2_Story_of_the_Medals.py
+++ pages/2_Story_of_the_Medals.py
@@ -45,7 +45,11 @@ view_type = st.sidebar.radio("**Select view option**", options = ['Country','Spo
 medal_df = create_medal_tally(df,view_type)
 
 #Creating sorted field list based on Total count for visualisation
-sorted_field = medal_df.sort_values('Total',ascending=False)[view_type].tolist()
+if 'Total' in medal_df.columns:
+    sorted_field = medal_df.sort_values('Total',ascending=False)[view_type].tolist()
+
+else:
+    sorted_field = medal_df.sort_values('🥇 Gold',ascending=False)[view_type].tolist()
 
 #Tabular output within expander
 with st.expander(f"**Medal Table by {view_type}**",expanded=True):
@@ -54,7 +58,7 @@ with st.expander(f"**Medal Table by {view_type}**",expanded=True):
     st.dataframe(medal_df,use_container_width=True,hide_index=True)
 
 #Stacked bar chart within expander
-with st.expander(f"**Visualisation of Top 10 by {view_type}**",expanded=True):
+with st.expander(f"**Top 10 Medal Performance by {view_type}**",expanded=True):
 
     st.write("\n")
     
@@ -63,16 +67,20 @@ with st.expander(f"**Visualisation of Top 10 by {view_type}**",expanded=True):
     medal_df = medal_df[medal_df[view_type].isin(top_10)]
 
     #Preparing dataframe for visualisation by converting wide format medal_df to long format
-    stacked_df = pd.melt(medal_df.drop(columns='Total'),id_vars=view_type,var_name = 'Medal', value_name = 'Count')
+    if 'Total' in medal_df.columns:
+        stacked_df = pd.melt(medal_df.drop(columns='Total'),id_vars=view_type,var_name = 'Medal', value_name = 'Count')
+    
+    else:
+        stacked_df = pd.melt(medal_df,id_vars=view_type,var_name = 'Medal', value_name = 'Count')
 
     #Creating medal tally visualisation
     medal_tally_vis(stacked_df,sorted_field,view_type)
 
-#This visualisation should only appear if All Games selected
-if games=='All':
+#This visualisation should only appear if All Games selected and more than one Olympics present
+if games=='All' and df['Olympics'].nunique()!=1:
 
     #Bar chart of gold medal winners over Olympic Games within expander
-    with st.expander(f"**Visualisation of Cumulative Gold Medals by {view_type} Across Olympic Games**",expanded=True):
+    with st.expander(f"**Olympic Games Progression: Gold Medals by {view_type}**",expanded=True):
             
         #Creating slider of Olympic games
         end_game = st.select_slider("Olympic Games",options = df['Olympics'].unique(),label_visibility='hidden')
@@ -100,34 +108,37 @@ if games=='All':
         #Creating dynamic visualisation with slider selection
         dynamic_medals_vis(stacked_df,sorted_field,view_type)
 
-#Bar chart of gold medals winners over Day of Olympic Games within expander
-with st.expander(f"**Visualisation of Cumulative Gold Medals by {view_type} Across Day of Olympic Games**",expanded=True):
+#This visualisation should only appear if more than one day of games present
+if df['Day of Games'].nunique()!=1:
 
-    #Creating slider of Day of Olympic games
-    end_day = st.select_slider("Day of Games",options = df['Day of Games'].unique().astype(int))
+    #Bar chart of gold medals winners over Day of Olympic Games within expander
+    with st.expander(f"**Day-by-day Progression: Gold Medals by {view_type}**",expanded=True):
 
-    #Creating list of all days from first to last selected
-    try:
-        all_days = list(df['Day of Games'].unique())[0:end_day+1]
-    except:
-        st.write("Error")
+        #Creating slider of Day of Olympic games
+        end_day = st.select_slider("Day of Games",options = df['Day of Games'].unique().astype(int))
 
-    #Filtering dataframe for selected Day of Olympic games and top 10 countries
-    day_games_df = df[(df['Day of Games'].isin(all_days)) & (df[view_type].isin(top_10))].copy()
+        #Creating list of all days from first to last selected
+        try:
+            all_days = list(df['Day of Games'].unique())[0:end_day+1]
+        except:
+            st.write("Error")
 
-    #Creating dynamic_chart_df
-    dynamic_chart_df = create_medal_tally(day_games_df,view_type)
+        #Filtering dataframe for selected Day of Olympic games and top 10 countries
+        day_games_df = df[(df['Day of Games'].isin(all_days)) & (df[view_type].isin(top_10))].copy()
 
-    #Preparing dataframe for visualisation by converting wide format medal_df to long format
-    stacked_df = pd.melt(dynamic_chart_df[[view_type,'🥇 Gold']], id_vars=view_type, value_name='Count')
-    stacked_df.drop(columns='Position',inplace=True)
-    stacked_df = stacked_df.sort_values(by='Count',ascending=False)
+        #Creating dynamic_chart_df
+        dynamic_chart_df = create_medal_tally(day_games_df,view_type)
 
-    #Creating sorted field based on gold medal count for visualisation
-    sorted_field = stacked_df[view_type].tolist()
+        #Preparing dataframe for visualisation by converting wide format medal_df to long format
+        stacked_df = pd.melt(dynamic_chart_df[[view_type,'🥇 Gold']], id_vars=view_type, value_name='Count')
+        stacked_df.drop(columns='Position',inplace=True)
+        stacked_df = stacked_df.sort_values(by='Count',ascending=False)
 
-    #Creating dynamic visualisation with slider selection
-    dynamic_medals_vis(stacked_df,sorted_field,view_type)
+        #Creating sorted field based on gold medal count for visualisation
+        sorted_field = stacked_df[view_type].tolist()
+
+        #Creating dynamic visualisation with slider selection
+        dynamic_medals_vis(stacked_df,sorted_field,view_type)
 
 #Creating page footer
 page_footer()
--- pages/3_Event_Performance.py
+++ pages/3_Event_Performance.py
@@ -21,17 +21,15 @@ df = st.session_state['df']
 event_performance_df = df[(df['Score'].notnull() | (df['Time(s)'].notnull()))].copy()
 
 #Secondary filtering to selected sports
-event_performance_df = event_performance_df[event_performance_df['sport'].isin(['Athletics','Swimming','Diving','Artistic Gymnastics'])]
+event_performance_df = event_performance_df[event_performance_df['sport'].isin(['Athletics','Swimming','Diving','Artistic Gymnastics','Canoe Slalom','Trampolining'])]
 
-#Creating columns to display subheader and radio buttons separately with suitable column spacing
-col1,col2,col3 = st.columns([10,5,7])
-
-with col1:
-    st.subheader("Event Performance")
+#Section descriptions
+st.subheader("Event Performance")
+st.write("Discover how Olympic champions' performances have evolved over the years, from record-breaking times to higher scores. Select a sport and event to see the trends and improvements among gold medalists across different Olympic Games.")
+st.write('\n')
 
 #Creating selectbox to choose sport
-with col2:
-    sport = st.selectbox("**Select sport**", index=None, options = sorted(event_performance_df['Sport'].unique()))
+sport = st.sidebar.selectbox("**Sport**", index=None, options = sorted(event_performance_df['Sport'].unique()))
 
 #Performs the below if sport selected
 if sport is not None:
@@ -40,8 +38,7 @@ if sport is not None:
     event_performance_df = event_performance_df[event_performance_df['Sport']==sport]
 
 #Creating selectbox to choose event name
-with col3:
-    event_name = st.selectbox("**Select event name**", index=None, options = sorted(event_performance_df['Event Name'].unique()))
+event_name = st.sidebar.selectbox("**Event Name**", index=None, options = sorted(event_performance_df['Event Name'].unique()))
 
 #Performs the below if event_name selected
 if event_name is not None:
